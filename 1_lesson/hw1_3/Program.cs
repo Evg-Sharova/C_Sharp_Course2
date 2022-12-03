7217c4194854c882b3b640cbb40379e809144dcb
Evgenya <avav82@mail.ru>
2022-12-03 17:13:06 +0500

    add hw

---
+++ 1_lesson/hw1_3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимет число и выдает,
+// является ли число четным (делится ли оно на два без остатка).
+
+int n_1 = int.Parse(Console.ReadLine());
+if (n_1 % 2 == 0)
+{
+    Console.Write($"The number {n_1} is even");
+}
+else
+{
+    Console.Write($"The number {n_1} is odd");
+}
+
