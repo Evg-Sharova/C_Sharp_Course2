deb8d67362c4db59c7dd404a1b57c0bd310d3615
Evgenya <avav82@mail.ru>
2022-12-05 15:05:52 +0500

    task 4_0, 4_1, 4_2, 4_3

---
+++ 4_lesson/4_2/Program.cs
@@ -0,0 +1,14 @@
+﻿// 2. Напишите программу, которая принимает на вход
+// число N и выдаёт произведение чисел от 1 до N.
+
+int Proiz(int num)
+{
+    int all_proiz = 1;
+    for (int i = 1; i <= num; i++)
+    {
+        all_proiz = all_proiz * i;
+    }
+    return all_proiz;
+}
+
+Console.WriteLine(Proiz(int.Parse(Console.ReadLine())));
