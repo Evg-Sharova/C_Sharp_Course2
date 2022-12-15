02678fabf990c587a1a9edbf32f305c30b4f45e8
Evgenya <avav82@mail.ru>
2022-12-15 15:34:19 +0500

    hw 6_1, 6_2

---
+++ 6_lesson/hw6_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// 1. Пользователь вводит с клавиатуры М чисел. Посчитайте, сколько чисел 
+//    больше нуля ввел пользователь.
+
+
+int CountNum()
+{
+    int count = 0;
+    string word;
+
+    while(true)
+    {
+        Console.Write("Введите число: ");
+        word = Console.ReadLine();
+
+        if (word == "") return count;
+        else if (int.Parse(word) > 0) count +=1;
+    }
+}
+
+Console.Write(CountNum());
