c351b8f631568ef8ae98bd8c2db9bdf2fdc61f79
Evgenya <avav82@mail.ru>
2022-12-22 12:41:32 +0500

    tasks and homeworks

---
+++ 9_lesson/9_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// 2. Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все натуральные числа в промежутке от M до N.
+
+void Numbers(int m, int n)
+{
+
+    if (m > n) return;
+
+    Console.Write($" {m} ");
+    Numbers(m+1, n);
+}
+
+Console.Write("Enter the number m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter the number n: ");
+int n = int.Parse(Console.ReadLine());;
+Numbers(m, n);
---
+++ 9_lesson/hw9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// 1. Задайте значения M и N. Напишите программу, которая
+// рекурсивно выведет все четные натуральные числа в промежутке от M до N.
+
+void Numbers(int m, int n)
+{
+
+    if (m > n) return;
+
+        if (m % 2 /= 1)
+        Console.Write($" {m} ");
+        Numbers(m+2, n);
+        
+}
+
+Console.Write("Enter the number m: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Enter the number n: ");
+int n = int.Parse(Console.ReadLine());;
+Numbers(m, n);
